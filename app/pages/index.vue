2a0e6ed1c1629ecb3be3357e080105844b0a6079
Egor <egorviliavin@gmail.com>
2024-06-24 22:53:07 +0300

    Fix: меняет текст

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-useSeoMeta({
-  title: 'АЛЬФА КУРС || Главная страница',
-})
+	useSeoMeta({
+		title: 'АЛЬФА КУРС || Главная страница',
+	});
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
+	<h1 class="text-3xl font-bold">Hello ALFA!!!</h1>
 </template>
